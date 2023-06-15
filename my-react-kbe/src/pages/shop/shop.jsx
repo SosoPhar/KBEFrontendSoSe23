29fa5205b8a200b8c82e9562f2d39f5ad5dc1502
SosoPhar <Sophia.Piyamit@Student.HTW-Berlin.de>
2023-06-15 22:38:26 +0200

    ShopBase + Button

--- my-react-kbe/src/pages/shop/shop.jsx
+++ my-react-kbe/src/pages/shop/shop.jsx
@@ -7,13 +7,14 @@ export const Shop = () => {
     return (
         <div className="shop">
             <div className="shopTitle">
-                <h1>YourArtLOVE</h1>
+                <h1>Your ArtLOVE</h1>
             </div>
             <div className="products">
                 {PRODUCTS.map((product) => (
                     <Product data={product} />
                 ))}
             </div>
+            <button className={"addToCartButton"}> Add To Cart </button>
         </div>
     );
 };
