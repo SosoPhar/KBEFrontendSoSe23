7996c70fba22230566f4b9fda165976e2d775851
skander_ayari <skander.ayari@ktc.de>
2023-09-17 21:48:43 +0200

    add SecuredPage

---
+++ my-react-kbe/src/pages/SecuredPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const SecuredPage = () => {
+
+    return (
+        <div>
+            <h1 >Welcome to the Protected Page.</h1>
+            <h1 >Welcome to the Protected Page.</h1>
+            <h1 >Welcome to the Protected Page.</h1>
+            <h1 >Welcome to the Protected Page.</h1>
+
+
+        </div>
+    );
+};
+
+export default SecuredPage;
