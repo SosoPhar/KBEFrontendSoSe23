dbf0b32c7b87cf1153860223647be7faeb5c86b3
skander_ayari <skander.ayari@ktc.de>
2023-09-27 23:25:46 +0200

    - Add rm from Cart Functionality

--- my-react-kbe/src/pages/shop/Product.jsx
+++ my-react-kbe/src/pages/shop/Product.jsx
@@ -7,7 +7,7 @@ export const Product = (props) => {
     const { isLogin, userData } = props;
 
     const { id, productName, productDescription, price, productImage } = props.data;
-    const { addToCart, cartItems } = useContext(ShopContext);
+    const { addToCart,removeFromCart, cartItems } = useContext(ShopContext);
     const cartItemCount = cartItems[id] || 0;
     const isAdmin = isLogin && userData?.username?.includes('admin');
 
@@ -19,10 +19,9 @@ export const Product = (props) => {
                 <p>{productDescription}</p>
                 <p><b>{price}</b></p>
 
-                {/* Conditionally render the "Delete" button for admin users */}
                 {isAdmin && (
-                    <button className="addToCartButton">
-                        Delete Cart
+                    <button className="addToCartButton" onClick={() => removeFromCart(id)}>
+                        Delete From Cart {cartItemCount > 0 && <> ({cartItemCount})</>}
                     </button>
                 )}
             </div>
